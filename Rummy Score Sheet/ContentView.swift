2bc84687590a98bbd0a57b7f48f9c1b2fdc9eb5e
vinodlingamsetty <vinodlingamsetty@gmail.com>
2026-01-30 15:38:47 -0500

    Initial Commit

---
+++ Rummy Score Sheet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rummy Score Sheet
+//
+//  Created by vin on 1/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rummy Score Sheet/Rummy_Score_SheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rummy_Score_SheetApp.swift
+//  Rummy Score Sheet
+//
+//  Created by vin on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct Rummy_Score_SheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
